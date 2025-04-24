7e3f6b631c15ac26be54c8ebddbdc5ca27ca01a0
joaomarcelo-java <diasdelutach@gmail.com>
2025-04-24 01:23:57 -0300

    Um programa que faz o calculo da área de um quadrado.

---
+++ AreaQuadrado.py
@@ -0,0 +1,4 @@
+tamLado = int(input("Digite o tamanho de um dos lados do quadrado(em metros), para sabermos a área total: "))
+area = tamLado ** 2
+
+print("A área do quadrado é de " + str(area) + " metros.")
