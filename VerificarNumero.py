e3724a82bb7733d1823f141e94f662cdf4565773
joaomarcelo-java <diasdelutach@gmail.com>
2025-04-24 01:53:38 -0300

    Um programa que diz se o número digitado é negativo ou positivo.

---
+++ VerificarNumero.py
@@ -0,0 +1,8 @@
+num1 = int(input("Digite um numero: "))
+
+if num1 > 0:
+    print("Este número é positivo.")
+elif num1 < 0:
+    print("Este número é negativo.")
+else:
+    print("Você digitou o número 0.")
