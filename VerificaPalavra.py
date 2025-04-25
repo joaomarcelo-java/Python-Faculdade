a61408c5011c34373964af5236fc279cd5384c59
joaomarcelo-java <diasdelutach@gmail.com>
2025-04-24 21:52:51 -0300

    Um programa que verifica se a palavra esta contida na frase digitada.

---
+++ VerificaPalavra.py
@@ -0,0 +1,7 @@
+frase = input("Digite uma frase: ")
+palavra = input("Digite uma palavra: ")
+
+if palavra in frase:
+    print("A palavra esta contida na frase.")
+else:
+    print("A palavra não esta contida na frase")
